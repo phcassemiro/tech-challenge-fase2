d0f133df0a0566ae2df2cb9609fea8e3116d7bce
Pedro Henrique Cassemiro da Silva <phcassemiro@hotmail.com>
2025-06-10 22:06:01 -0300

    Excluindo testes

--- test/app.test.js
+++
@@ -1,9 +0,0 @@
-const request = require('supertest');
-const app = require('../app'); // ou '../index' dependendo do seu projeto
-
-describe('Testando rota principal', () => {
-  it('deve responder com status 200', async () => {
-    const res = await request(app).get('/');
-    expect(res.statusCode).toBe(200);
-  });
-});
